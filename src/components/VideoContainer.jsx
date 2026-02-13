d71c05b97881d8ebc4f52dfc8c6f416bacbeb6ff
amanuniyal01 <amanuniyal315@gmail.com>
2026-02-13 13:43:19 +0530

    Added promoted label by using HOC

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -4,38 +4,44 @@ import VideoCard from './VideoCard';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { SetVideos } from '../utils/VideoSlice';
+import withPromotedLabel from './withPromotedLabel';
 
-
+const PromotedVideoCard = withPromotedLabel(VideoCard);
 
 function VideoContainer() {
   const videos = useSelector(store => store.videos.videos) || []
   const dispatch = useDispatch()
+  const isMenuOpen = useSelector(store => store.app.isMenuOpen);
 
   useEffect(() => {
     getVideos();
-
   }, [])
 
-  const isMenuOpen = useSelector(store => store.app.isMenuOpen);
   const getVideos = async () => {
-
     const data = await fetch(YOUTUBE_API);
     const json = await data.json()
-    console.log(json.items)
     dispatch(SetVideos(json.items))
-
   }
 
   return (
-    <div className={`flex  flex-wrap justify-center gap-6 ${isMenuOpen ? "md:ml-60" : "md:ml-20"} mt-16 px-6`}>
-      {videos?.map((video) => (
-        <Link key={video.id} to={`/watch?v=${video.id}`}>
-          <VideoCard info={video} />
-        </Link>
-      ))}
+    <div className={`flex flex-wrap justify-center gap-6 ${isMenuOpen ? "md:ml-60" : "md:ml-20"} mt-16 px-6`}>
+      {videos?.map((video) => {
+        const isPromoted =
+          Number(video?.statistics?.viewCount) > 1000000;
+
+        return (
+          <Link key={video.id} to={`/watch?v=${video.id}`}>
+            {isPromoted
+              ? <PromotedVideoCard info={video} />
+              : <VideoCard info={video} />
+            }
+          </Link>
+        )
+      })}
     </div>
   );
 }
+
 
 export default VideoContainer
 
