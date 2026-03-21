df820980224a863ce14ca277fb5856012f6beced
amanuniyal01 <amanuniyal315@gmail.com>
2026-03-21 20:37:07 +0530

    Added shimmer on results Page

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,21 +3,25 @@ import { Link, useSearchParams } from 'react-router-dom'
 import { GOOGLE_API, YOUTUBE_SEARCH_API, YOUTUBE_SEARCH_RESULTS_API } from '../utils/constant'
 import ResultCard from './ResultCard'
 import { useSelector } from 'react-redux'
+import Shimmer from './Shimmer'
 
 function Results() {
     const [videos, SetVideos] = useState([])
+    const [loading, setLoading] = useState(false)
     const isMenuOpen = useSelector(store => store.app.isMenuOpen);
 
     const [searchParams] = useSearchParams()
     const query = searchParams.get("q")
 
     const fetchVideos = async () => {
+        setLoading(true)
         const data = await fetch(YOUTUBE_SEARCH_RESULTS_API +
             encodeURIComponent(query) +
             "&key=" + GOOGLE_API)
         const json = await data.json()
         console.log(json)
         SetVideos(json.items)
+        setLoading(false)
     }
 
     useEffect(() => {
@@ -25,22 +29,29 @@ function Results() {
             fetchVideos()
         }
     }, [query])
-
+    if (!videos) return <h1>"Loading"</h1>
     return (
-        <div className={isMenuOpen?"ml-65":"ml-50" } >
+        <div className={isMenuOpen ? "ml-65" : "ml-50"}>
 
             <div className='mt-20'>
-                {videos.map((video) => (
-                    <Link
-                        key={video.id.videoId}
-                        to={"/watch?v=" + video.id.videoId}
-                    >
-                        <ResultCard info={video} />
-                    </Link>
-                ))}
+
+                {loading ? (
+
+                    <Shimmer />
+                ) : (
+                    videos.map((video) => (
+                        <Link
+                            key={video.id.videoId}
+                            to={"/watch?v=" + video.id.videoId}
+                        >
+                            <ResultCard info={video} />
+                        </Link>
+                    ))
+                )}
+
             </div>
         </div>
-    )
+    );
 }
 
 export default Results
---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,16 @@
+function Shimmer() {
+    return (
+        <div className="p-4">
+            {Array(10).fill("").map((_, index) => (
+                <div key={index} className="flex gap-4 mb-4 animate-pulse">
+                    <div className="w-40 h-24 bg-gray-300 rounded"></div>
+                    <div className="flex-1 space-y-2">
+                        <div className="h-4 bg-gray-300 rounded w-3/4"></div>
+                        <div className="h-4 bg-gray-300 rounded w-1/2"></div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+export default Shimmer;
