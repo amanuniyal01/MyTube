1d59f4e64b20c69300f30f0f2bb8522628e5b657
amanuniyal01 <amanuniyal315@gmail.com>
2026-03-28 15:14:02 +0530

    APi Polling working fine on console

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ChatMessage from './ChatMessage'
 
 function LiveChat() {
+    useEffect(() => {
+        const i = setInterval(() => {
+            // API POLLING
+        console.log("API POLLING")
+
+        }, 2000)
+        return () => clearInterval(i)
+    }, [])
     return (
+
+
         <div className=' w-full h-[500px] rounded-lg bg-slate-100  border-black border-solid border-2'>
             <ChatMessage name="Aman Uniyal" message="Hello Everyone" />
         </div>
