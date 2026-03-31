f88d7dd74536267e839165c394f1ce0af93ff80f
amanuniyal01 <amanuniyal315@gmail.com>
2026-03-31 12:33:45 +0530

    Added send chat button in live chat

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ChatMessage from './ChatMessage'
 import { useDispatch, useSelector } from 'react-redux'
 import { addMessage } from '../utils/ChatSlice';
@@ -6,6 +6,15 @@ import { generateRandomMessage, generateRandomName } from '../utils/helper';
 
 function LiveChat() {
     const dispatch = useDispatch();
+    const [input, setInput] = useState("")
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        dispatch(addMessage({
+            name: generateRandomName(),
+            message: input
+        }))
+
+    }
     const chatMessages = useSelector(store => store.chat.messages)
     useEffect(() => {
         const i = setInterval(() => {
@@ -20,12 +29,17 @@ function LiveChat() {
     }, [])
     return (
 
-
-        <div className='flex flex-col-reverse overflow-y-scroll overflow-hidden w-full h-[500px] rounded-lg bg-slate-100  border-black border-solid border-2'>
+        <>        <div className='flex flex-col-reverse overflow-y-scroll overflow-hidden w-full h-[500px] rounded-lg bg-slate-100  border-black border-solid border-2'>
             {chatMessages.map((c, index) =>
                 <ChatMessage key={index} name={c.name} message={c.message} />)
             }
         </div>
+            <form onSubmit={handleSubmit}>
+                <input value={input} onChange={(e)=>setInput(e.target.value)}className='p-2 bg-transparent' placeholder='Enter your message' />
+                <button>Send</button>
+            </form>
+        </>
+
     )
 }
 
