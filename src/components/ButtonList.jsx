837c05ad3c24ff1abc082a01fb658eb81e6aa7c9
amanuniyal01 <amanuniyal315@gmail.com>
2026-04-04 21:23:11 +0530

    Added dark/light mode On Button list

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -6,9 +6,10 @@ function ButtonList() {
   const langKey = useSelector((store) => store.config.lang)
   const categories = language[langKey].categories
   const isMenuOpen = useSelector(store => store.app.isMenuOpen);
+  const isDarkMode=useSelector((store)=>store.theme.darkMode)
 
   return (
-    <div className={`flex mt-20   overflow-x-auto ${isMenuOpen?"ml-65":"ml-10"} whitespace-nowrap p-2 bg-white scrollbar-hide`}>
+    <div className={`flex mt-20   overflow-x-auto ${isMenuOpen?"ml-65":"ml-10"} whitespace-nowrap p-2 ${isDarkMode?"bg-gray-800":"bg-white"}  scrollbar-hide`}>
       {categories.map((name, index) => (
         <Button key={index} name={name} />
       ))}
