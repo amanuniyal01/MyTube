7172c5ee2d28f217f54a6405ea7fce0afd0265f5
amanuniyal01 <amanuniyal315@gmail.com>
2026-04-05 11:57:26 +0530

    Added dark/light mode on each btn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Button({ name }) {
+  const isDarkMode = useSelector((store) => store.theme.darkMode)
   return (
     <button
-      className="px-4  shrink-0 py-2 m-2 bg-gray-200 text-gray-800 font-medium rounded-lg 
-                 hover:bg-gray-300 hover:cursor-pointer active:bg-gray-400 transition-all duration-200"
+      className={`px-4 ${isDarkMode?"bg-gray-500 text-white":"bg-gray-200"}  shrink-0 py-2 m-2  text-gray-800 font-medium rounded-lg 
+                 hover:bg-gray-300 hover:cursor-pointer active:bg-gray-400 transition-all duration-200`}
     >
       {name}
     </button>
